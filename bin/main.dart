f00ae6eb209a74ee75616a486264ff29e5969b3d
Kiana <160209328+Ttactic@users.noreply.github.com>
2025-01-28 12:45:36 +0330

    files

---
+++ bin/main.dart
@@ -0,0 +1,39 @@
+void main() {
+  num inputMonth = 6;
+  if (inputMonth == 1) {
+    print("January");
+  }
+  else if (inputMonth == 2) {
+    print("February");
+  }
+  else if  (inputMonth == 3) {
+    print("March");
+  }
+  else if  (inputMonth == 4) {
+    print("April");
+  }
+  else if  (inputMonth == 5) {
+    print("May");
+  }
+  else if  (inputMonth == 6) {
+    print("June");
+  }
+  else if  (inputMonth == 7) {
+    print("July");
+  }
+  else if (inputMonth == 8) {
+    print("August");
+  }
+  else if  (inputMonth == 9) {
+    print("September");
+  }
+  else if (inputMonth == 10) {
+    print("October");
+  }
+  else if (inputMonth == 11) {
+    print("November");
+  }
+  else if (inputMonth == 12) {
+    print("December");
+  }
+}
